9f75a361e2ac85554be79f5f867166428349128a
Aman Singh <singhaman38809@gmail.com>
2023-11-26 10:56:24 +0530

    first commit

---
+++ js/trailers.js
@@ -0,0 +1,7 @@
+
+const toWatchtrailer = document.getElementById("toWatch")
+// console.log(toWatchtrailer);
+toWatchtrailer.addEventListener("click", () => {
+    window.location = "https://www.youtube.com/watch?v=1JLUn2DFW4w/autoplay"
+    // window.focus();
+})
